651963dbfb3bbb34e92761ae983bf587335b9869
HCI UW <wisconsinhci@gmail.com>
2019-11-15 18:43:15 -0600

    debugging

--- cobots_ur_bringup/src/joint_state_republisher.py
+++ cobots_ur_bringup/src/joint_state_republisher.py
@@ -9,12 +9,26 @@ from sensor_msgs.msg import JointState
 class JointStateRepublisher:
 
     def __init__(self, rosbridge_host, rosbridge_port, bridge_name_prefix):
+        self._count = 0
+        
         self._bridge_client = roslibpy.Ros(host=rosbridge_host,port=rosbridge_port)
+
+        not_setup = True
+        while not rospy.is_shutdown() and not_setup:
+            try:
+                self._bridge_client.run()
+                not_setup = False
+            except:
+                print 'Waiting for ROSBridge to connect'
+            rospy.sleep(0.25)
+        print 'ROSBridge connected'
 
         self._joint_state_sub = rospy.Subscriber('{}/joint_states'.format(bridge_name_prefix),JointState,self._joint_state_bridge_cb)
         self._joint_state_pub = roslibpy.Topic(self._bridge_client, '{}/joint_states'.format(bridge_name_prefix), 'sensor_msgs/JointState')
 
     def _joint_state_bridge_cb(self, msg):
+        print self._count, '::::', msg
+        self._count += 1
         self._joint_state_pub.publish({
             'name': msg.name,
             'position': msg.position,
