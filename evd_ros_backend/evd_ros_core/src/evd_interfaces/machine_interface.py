6c72f95cfcc630e6c55fe681cab842b7c68d36c6
Curt Henrichs <cdhenrichs@wisc.edu>
2021-05-12 19:05:24 -0500

    First pass implementation of machine itnerface + creation of machine messages + refactoring name of robot messages ~ note codebase is currently broken

--- evd_ros_backend/evd_ros_core/src/evd_interfaces/machine_interface.py
+++ evd_ros_backend/evd_ros_core/src/evd_interfaces/machine_interface.py
@@ -1,34 +1,62 @@
 
 import rospy
 
+from evd_ros_core.msg import MachineAck, MachineInitialize, MachinePause, MachineStart, MachineStop, MachineStatus
+
 
 class MachineInterface:
 
-    def __init__(self, prefix):
+    def __init__(self, prefix=None):
         self._prefix = prefix
+        prefix_fmt = prefix+'/' if prefix != None else ''
+        self._status_table = {}
+        self._ack_table = {}
 
-        self.start_pub
-        self.stop_pub
-        self.pause_pub
+        self.initialize_pub = rospy.Publisher('{0}machine/initialize'.format(prefix_fmt), MachineInitialize, queue_size=10)
+        self.start_pub = rospy.Publisher('{0}machine/start'.format(prefix_fmt), MachineStart, queue_size=10)
+        self.stop_pub = rospy.Publisher('{0}machine/stop'.format(prefix_fmt), MachineStop, queue_size=10)
+        self.pause_pub = rospy.Publisher('{0}machine/pause'.format(prefix_fmt), MachinePause, queue_size=10)
 
-        self.ack_sub
-        self.wait_sub
+        self.ack_sub = rospy.Subscriber('{0}machine/ack'.format(prefix_fmt), MachineAck, self._ack_cb)
+        self.status_sub = rospy.Subscriber('{0}machine/wait'.format(prefix_fmt), MachineStatus, self._status_cb)
 
+    def _ack_cb(self, msg):
+        self._ack_table[msg.uuid] = msg.ack
+
+    def _status_cb(self, msg):
+        self._status_table[msg.uuid] = {'running': msg.running, 'status': msg.status}
 
     def estop(self, machineUuid):
-        pass
+        msg = MachineStop(machineUuid,True)
+        self.stop_pub.publish(msg)
 
     def pause(self, state, machineUuid):
-        pass
+        msg = MachinePause(machineUuid,state)
+        self.pause_pub.publish(msg)
 
     def initialize(self, machineUuid):
-        pass
+        msg = MachineInitialize(machineUuid)
+        self.initialize_pub.publish(msg)
 
     def start(self, machineUuid):
-        pass
+        msg = MachineStart(machineUuid)
+        self.start_pub.publish(msg)
 
     def stop(self, machineUuid):
-        pass
+        msg = MachineStop(machineUuid,False)
+        self.stop_pub.publish(msg)
+
+    def is_acked(self, machineUuid, clearEntry=True):
+        if not machineUuid in self._ack_table.keys():
+            return None
+        else:
+            ack = self._ack_table[machineUuid]
+            if clearEntry:
+                del self._ack_table[machineUuid]
+            return ack
 
     def get_status(self, machineUuid):
-        return 'running' #TODO
+        if not machineUuid in self._status_table.keys():
+            return None
+        else:
+            return self._status_table[machineUuid]
--- evd_ros_backend/evd_ros_core/src/evd_interfaces/machine_template.py
+++ evd_ros_backend/evd_ros_core/src/evd_interfaces/machine_template.py
@@ -1,1 +1,106 @@
 # TODO this abstracts the publishing to machines (all task machines should use this!)
+import rospy
+
+from evd_ros_core.msg import MachineAck, MachineInitialize, MachinePause, MachineStart, MachineStop, MachineStatus
+
+
+class MachineTemplate:
+
+    VALID_STATES = [
+        MachineStatus.STATUS_RUNNING,
+        MachineStatus.STATUS_IDLE,
+        MachineStatus.STATUS_PAUSED,
+        MachineStatus.STATUS_ERROR
+    ]
+
+
+    def __init__(self, uuid='', prefix=None, init_fnt=None, start_fnt=None, stop_fnt=None, pause_fnt=None):
+        self.uuid = uuid
+        self._prefix = prefix
+        prefix_fmt = prefix+'/' if prefix != None else ''
+        self._running = False
+        self._status = 'unknown'
+
+        self._init_fnt = init_fnt
+        self._start_fnt = start_fnt
+        self._stop_fnt = stop_fnt
+        self._pause_fnt = pause_fnt
+
+        self.ack_pub = rospy.Publisher('{0}machine/ack'.format(prefix_fmt), MachineAck, queue_size=10)
+        self.status_pub = rospy.Publisher('{0}machine/wait'.format(prefix_fmt), MachineStatus, queue_size=10)
+
+        self.initialize_sub = rospy.Subscriber('{0}machine/initialize'.format(prefix_fmt), MachineInitialize, self._initialize_cb)
+        self.start_sub = rospy.Subscriber('{0}machine/start'.format(prefix_fmt), MachineStart, self._start_cb)
+        self.stop_sub = rospy.Subscriber('{0}machine/stop'.format(prefix_fmt), MachineStop, self._stop_cb)
+        self.pause_sub = rospy.Subscriber('{0}machine/pause'.format(prefix_fmt), MachinePause, self._pause_cb)
+
+    def _initialize_cb(self, msg):
+        if self.uuid == msg.uuid:
+
+            # Call machine routine
+            ack = True
+            if self._init_fnt != None:
+                ack = self._init_fnt() == True
+
+            # Generate ACK/NACK
+            retmsg = MachineAck(self.uuid,ack)
+            self.ack_pub.publish(retmsg)
+
+    def _start_cb(self, msg):
+        if self.uuid == msg.uuid:
+
+            # Call machine routine
+            ack = True
+            if self._start_fnt != None:
+                ack = self._start_fnt() == True
+
+            # Generate ACK/NACK
+            retmsg = MachineAck(self.uuid,ack)
+            self.ack_pub.publish(retmsg)
+
+    def _stop_cb(self, msg):
+        if self.uuid == msg.uuid:
+
+            # Call machine routine
+            ack = True
+            if self._stop_fnt != None:
+                ack = self._stop_fnt(msg.emergency) == True
+
+            # Generate ACK/NACK
+            retmsg = MachineAck(self.uuid,ack)
+            self.ack_pub.publish(retmsg)
+
+    def _pause_cb(self, msg):
+        if self.uuid == msg.uuid:
+
+            # Call machine routine
+            ack = True
+            if self._pause_fnt != None:
+                ack = self._pause_fnt(msg.state) == True
+
+            # Generate ACK/NACK
+            retmsg = MachineAck(self.uuid,ack)
+            self.ack_pub.publish(retmsg)
+
+    @property
+    def is_running(self):
+        return self._running
+
+    @property
+    def current_status(self):
+        return self._status
+
+    @current_status.setter
+    def current_status(self, value):
+        
+        if value not in self.VALID_STATES:
+            raise Exception('Invalid status state specified: {0}'.format(value))
+        
+        self._status = value
+        if self._status == MachineStatus.STATUS_RUNNING:
+            self._running = True
+
+    def update_status(self):
+        msg = MachineStatus(self.uuid,self.is_running,self.current_status)
+        self.status_pub.publish(msg)
+
