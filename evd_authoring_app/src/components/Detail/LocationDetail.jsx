9a283bdfb353e5c7451deb1ecf57b4bf81b28264
Andrew Schoen <schoen.andrewj@gmail.com>
2021-06-03 09:59:15 -0500

    Made the detail more generic, but customizable WRT the inner content

--- evd_authoring_app/src/components/Detail/LocationDetail.jsx
+++ evd_authoring_app/src/components/Detail/LocationDetail.jsx
@@ -1,84 +1,21 @@
 import React, {useCallback} from 'react';
 
-import { Drawer, Empty, Input, Space, Button,Popconfirm } from 'antd';
-
-import useGuiStore from '../../stores/GuiStore';
+import { Empty } from 'antd';
 
 import useEvdStore from '../../stores/EvdStore';
 
-export const LocationDetail = (_) => {
-
-    const {focusItem, clearFocusItem} = useGuiStore(state=>({
-        focusItem:state.focusItem,
-        clearFocusItem:state.clearFocusItem
-    }));
+export const LocationDetail = ({uuid}) => {
 
     const {location} = useEvdStore(useCallback(state=>({
-        location:state.environment.locations[focusItem.uuid]
+        location:state.environment.locations[uuid]
     })
-      ,[focusItem]))
+      ,[uuid]))
     const { deleteItem, setItemProperty } = useEvdStore(state=>({
         deleteItem:state.deleteItem,
         setItemProperty:state.setItemProperty
     }));
-    const [visible, setVisible] = React.useState(false);
-    const handleOK = () =>{
-      clearFocusItem();
-      deleteItem('location',focusItem.uuid);
-    }
-    const handleCancel = () =>{
-       setVisible(false);
-    }
 
-    if (location) {
-        return (
-            <Drawer
-                title={
-                    <Space>
-                        <span>Location: </span>
-                        <Input
-                            defaultValue={location.name}
-                            disabled={!location.canEdit}
-                            onChange={e=>setItemProperty('location',focusItem.uuid,'name',e.target.value)}/>
-                    </Space>}
-                visible={focusItem.type === 'location'}
-                onClose={clearFocusItem}
-                getContainer={false}
-                style={{ position: 'absolute' }}
-                footer={
-                  <Popconfirm title= "Are you sure you want to delete this location?"
-                              onConfirm={handleOK}
-                              onCancel ={handleCancel}
-                              visible = {visible}
-                              placement ="top">
-                    <Button
-                        danger
-                        block
-                        disabled={!location.deleteable}
-                        onClick={()=>setVisible(true)}
-                    >
-                        Delete
-                    </Button>
-                    </Popconfirm>
-                }
-                width='50%'
-            >
-                <Empty/>
-            </Drawer>
-        )
-    } else {
-        return  (
-            <Drawer
-                title={'Location: '}
-                visible={focusItem.type === 'location'}
-                onClose={clearFocusItem}
-                getContainer={false}
-                style={{ position: 'absolute' }}
-                width='50%'
-            >
-                <Empty/>
-            </Drawer>
-        )
-
-    }
+    console.log(location);
+    
+    return (<Empty>Custom content for Location</Empty>)
 }
--- evd_authoring_app/src/components/Detail/index.jsx
+++ evd_authoring_app/src/components/Detail/index.jsx
@@ -1,25 +1,104 @@
-import React, { Fragment } from "react";
+import React, {useCallback} from 'react';
 
-import { LocationDetail } from "./LocationDetail";
-import {MachineDetail} from "./MachineDetail";
-import {WaypointDetail} from "./WaypointDetail";
-import {RegionDetail} from "./RegionDetail.jsx";
-import {ThingDetail} from "./ThingDetail.jsx";
+import { Drawer, Input, Empty, Space, Button, Popconfirm } from 'antd';
 
+import useGuiStore from '../../stores/GuiStore';
 
-export const Detail = (props) => {
+import { LocationDetail } from './LocationDetail';
+import { MachineDetail } from './MachineDetail';
+import { ThingDetail } from './ThingDetail';
+import { WaypointDetail } from './WaypointDetail';
 
-    return (
-        <Fragment>
+import useEvdStore from '../../stores/EvdStore';
 
-            <LocationDetail/>
-            <MachineDetail/>
-            <WaypointDetail/>
-            <ThingDetail/>
-            {/* <MachineDetail/>
-            <WaypointDetail/>
-            <RegionDetail/> */}
+export const Detail = (_) => {
 
-        </Fragment>
-    );
-};
+    const {focusItem, clearFocusItem} = useGuiStore(state=>({
+        focusItem:state.focusItem,
+        clearFocusItem:state.clearFocusItem
+    }));
+
+    const {item} = useEvdStore(useCallback(state=>({
+        item:focusItem.type ? state.environment[focusItem.type+'s'][focusItem.uuid] : null
+    }),[focusItem]))
+
+    const { deleteItem, setItemProperty } = useEvdStore(state=>({
+        deleteItem:state.deleteItem,
+        setItemProperty:state.setItemProperty
+    }));
+
+    const [popoverVisible, setPopoverVisible] = React.useState(false);
+      
+    const handleOK = () =>{
+        clearFocusItem();
+        deleteItem(focusItem.type,focusItem.uuid);
+    }
+    const handleCancel = () =>{
+        setPopoverVisible(false);
+    }
+
+    if (item) {
+        return (
+            <Drawer
+                title={
+                    <Space>
+                        <span style={{textTransform:'capitalize'}}>{focusItem.type} </span>
+                        <Input
+                            defaultValue={item.name}
+                            disabled={!item.editable}
+                            onChange={e=>setItemProperty(focusItem.type,focusItem.uuid,'name',e.target.value)}/>
+                    </Space>}
+                visible={focusItem.uuid !== null && focusItem.type !== null}
+                onClose={clearFocusItem}
+                getContainer={false}
+                style={{ position: 'absolute' }}
+                footer={
+                  <Popconfirm title={`Are you sure you want to delete this ${focusItem.type}?`}
+                              onConfirm={handleOK}
+                              onCancel ={handleCancel}
+                              visible = {popoverVisible}
+                              placement ="top">
+                    <Button
+                        danger
+                        block
+                        disabled={!item.deleteable}
+                        onClick={()=>setPopoverVisible(true)}
+                    >
+                        Delete
+                    </Button>
+                    </Popconfirm>
+                }
+                width='50%'
+            >
+                {focusItem.type === 'location' && (
+                    <LocationDetail uuid={focusItem.uuid}/>
+                )}
+                {focusItem.type === 'machine' && (
+                    <MachineDetail uuid={focusItem.uuid}/>
+                )}
+                {focusItem.type === 'waypoint' && (
+                    <WaypointDetail uuid={focusItem.uuid}/>
+                )}
+                {focusItem.type === 'thingType' && (
+                    <ThingDetail uuid={focusItem.uuid}/>
+                )}
+                {focusItem.type === null && (
+                    <Empty/>
+                )}
+            </Drawer>
+        )
+    } else {
+        return  (
+            <Drawer
+                title={<span style={{textTransform:'capitalize'}}>{focusItem.type} </span>}
+                visible={focusItem.uuid !== null && focusItem.type !== null}
+                onClose={clearFocusItem}
+                getContainer={false}
+                style={{ position: 'absolute' }}
+                width='50%'
+            >
+            </Drawer>
+        )
+
+    }
+}
