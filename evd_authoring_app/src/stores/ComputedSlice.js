c6809073cefa28c5990509af6c66f2b506994873
hunterZh37 <zzhang978@wisc.edu>
2021-09-15 20:24:27 -0500

    added color to business and quality frames

--- evd_authoring_app/src/stores/ComputedSlice.js
+++ evd_authoring_app/src/stores/ComputedSlice.js
@@ -77,7 +77,9 @@ export const ComputedSlice = {
                     //console.log("entered");
                     color = reachabilityColor(focused||trajectoryFocused,'location');
                     
-                }else{
+                }else if(this.frame === 'quality' || this.frame === 'business'){
+                    color = reachabilityColor(focused||trajectoryFocused,'location');
+                } else{
                     color = poseToColor(item, this.frame, focused || trajectoryFocused);
                 }
                 if (trajectoryFocused){       
@@ -106,6 +108,8 @@ export const ComputedSlice = {
                 if (item.joints.reachable && this.frame === 'performance'){//pose, frame, focused, locationOrWaypoint
                     color = reachabilityColor(item,this.frame,focused||trajectoryFocused,'waypoint');
                     
+                }else if(this.frame === 'quality' || this.frame === 'business'){
+                    color = reachabilityColor(focused||trajectoryFocused,'waypoint');
                 }else{
                     color = poseToColor(item, this.frame, focused || trajectoryFocused);
                 }
