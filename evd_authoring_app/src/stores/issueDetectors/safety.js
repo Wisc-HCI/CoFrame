85638f88a2d7a7022fafa6c7f5780bbb308dcf60
hunterZh37 <zzhang978@wisc.edu>
2021-09-24 13:57:53 -0500

    thing movement

--- evd_authoring_app/src/stores/issueDetectors/safety.js
+++ evd_authoring_app/src/stores/issueDetectors/safety.js
@@ -28,15 +28,15 @@ export const findThingMovementIssues = ({program,unrolled}) => { // May require 
     
     Object.values(unrolled).forEach(primitive=>{
         
-        //console.log(primitive.type);
+        console.log(primitive.type);
         if (primitive.type === 'node.primitive.gripper.' && primitive.parameters.semantic === 'grasping'){
             const thingUUID = primitive.parameters.thing_uuid.uuid;
-           // console.log(thingUUID + "  123" );
+            console.log(thingUUID + "  123" );
             Object.values(program.data.placeholders).forEach(placeholder => {
-                //console.log(placeholder.uuid );
+                console.log(placeholder.uuid );
                 if (placeholder.uuid === thingUUID){
                   const thingTypeUUID = placeholder.pending_node.thing_type_uuid;
-                  //console.log("true");
+                  console.log("true");
                   Object.values(program.data.thingTypes).forEach(thingType => {
                     console.log(thingType.uuid + "   " + thingTypeUUID);
                       if(thingType.uuid === thingTypeUUID && thingType.is_safe === false){
@@ -45,8 +45,8 @@ export const findThingMovementIssues = ({program,unrolled}) => { // May require 
                         issues[uuid] = {
                         uuid: uuid,
                         requiresChanges: true,
-                        title: `Graspping on unsafe object`,
-                        description: `The robot is graspping on a dangerous item`,
+                        title: `Grasping on unsafe object`, // might need some changes
+                        description: `The robot is grasping on a dangerous item`,// might need some changes
                         complete: false,
                         focus: {uuid:primitive.uuid, type:'primitive'},
                         graphData: null
