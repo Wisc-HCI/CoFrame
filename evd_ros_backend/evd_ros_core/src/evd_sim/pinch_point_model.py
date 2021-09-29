7c5d00e40cdf1e4750ee8c433dc5b47449023ccb
Curt Henrichs <cdhenrichs@wisc.edu>
2021-09-28 21:38:34 -0700

    updating lively config since it seems it was acting weird before + adding gripper link to urdf

--- evd_ros_backend/evd_ros_core/src/evd_sim/pinch_point_model.py
+++ evd_ros_backend/evd_ros_core/src/evd_sim/pinch_point_model.py
@@ -1,4 +1,8 @@
 '''
+Okay so here we are finally getting to pinch points
+
+What are they they are a separation distance that is near the threat level for a human and can evolve over time
+Effectively
 '''
 
 
