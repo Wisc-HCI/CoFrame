ad56018666c29958192515fcb74cc2abaac3a34e
Andrew Schoen <schoen.andrewj@gmail.com>
2022-02-19 11:40:44 -0600

    Update Tile.jsx

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Row } from 'antd';
+import { useSpring, animated } from "@react-spring/web";
+import { config } from "react-spring";
+
 
 export default function Tile({style, header, children, backgroundColor, borderWidth, internalPaddingWidth}) {
+
+    const innerContainerStyle = useSpring({
+        backgroundColor:backgroundColor?backgroundColor:'rgba(0,0,0,0.6)',
+        padding: internalPaddingWidth !== null ? internalPaddingWidth : 10,
+        config: config.molasses
+    })
 
     return (
         <div style={{backgroundColor:'rgba(100,100,100,0.3)', padding: borderWidth, borderRadius: 3,...style}}>
             {header && (
-                <Row style={{paddingBottom:borderWidth,width:'100%'}} justify='space-between'>
+                <div style={{paddingBottom:borderWidth,width:'100%'}} justify='space-between'>
                     {header}
-                </Row>
+                </div>
             )}
-            <div style={{backgroundColor:backgroundColor?backgroundColor:'rgba(0,0,0,0.6)', padding: internalPaddingWidth !== null ? internalPaddingWidth : 10, borderRadius: 3, color:'white'}}>
+            <animated.div style={{...innerContainerStyle, borderRadius: 3, color:'white'}}>
                 {children}
-            </div>
+            </animated.div>
         </div>
     )
 }
