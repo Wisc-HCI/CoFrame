2ca6b0ffb656d6b135dc2c76b43f1cac09687ec5
hunterZh37 <zzhang978@wisc.edu>
2022-04-02 12:32:38 -0500

    able to update the robot/gripper state

--- src/components/Detail/JointGripperInput.jsx
+++ src/components/Detail/JointGripperInput.jsx
@@ -1,35 +1,43 @@
-import React, { useState } from 'react';
-import { Box, DropButton, Text, Button } from "grommet";
-import { FormEdit } from "grommet-icons";
-import { eulerFromQuaternion, quaternionFromEuler } from './Geometry';
+import React from 'react';
+import { Box } from "grommet";
+
 import useStore from '../../stores/Store';
-import { FiX } from 'react-icons/fi';
 import Collapse from '../Collapse';
 import { NumberInput } from '../NumberInput';
 
 
 function JointGripperInput({ robotID, isGripper }) {
     let initialStateInfo = [];
+    let initialState = {};
+    let initialStateValue;
     if (isGripper === true) {
-        const typeInfo = useStore(state => state.programSpec.objectTypes.gripperType);
-         initialStateInfo = useStore(state => {
-            state.programData[robotID].properties.initialGripState;
-        })
+
+        initialStateValue = useStore(state =>
+            state.programData[robotID].properties.initialGripState
+        );
 
     } else {
-        const typeInfo = useStore(state => state.programSpec.objectTypes.robotAgentType);
         initialStateInfo = useStore(state => {
             let list = [];
             for (const [key, value] of Object.entries(state.programData[robotID].properties.initialJointState)) {
                 list.push({ key, value });
             }
-            console.log(list);
-            return list;         
-    })
-    }
-    const primaryColor = useStore(state => state.primaryColor);
+            return list;
+        })
+        initialState = useStore(state =>
+            state.programData[robotID].properties.initialJointState
+        );
 
-    const addFocusItem = useStore(state => state.addFocusItem);
+
+
+
+    }
+
+    console.log("initial", initialStateValue);
+
+    const updateItemSimpleProperty = useStore(state => state.updateItemSimpleProperty);
+
+
 
     if (isGripper) {
         return (
@@ -39,11 +47,10 @@ function JointGripperInput({ robotID, isGripper }) {
                     <Box direction='row' >
                         <div style={{ paddingLeft: "10%" }}>
                             <NumberInput
-                                value={initialStateInfo}
+                                value={initialStateValue}
                                 min={0}
                                 max={Infinity}
-                            // onChange={(value) => updateItemSimpleProperty(item.id, 'processTime', value)}
-
+                                onChange={(value) => updateItemSimpleProperty(robotID, 'initialGripState', value)}
                             />
                         </div>
                     </Box>
@@ -64,7 +71,7 @@ function JointGripperInput({ robotID, isGripper }) {
                         <b style={{ color: 'rgba(255, 255, 255, 0.85)' }} >Initial Joint States : </b>}{' '}
                     </Box>}
                 >
-                    { initialStateInfo.map((io, i) => {
+                    {initialStateInfo.map((io, i) => {
 
                         return (
                             <Box key={i} round="xsmall" background="rgba(100,100,100,0.3)" direction='row'
@@ -72,16 +79,17 @@ function JointGripperInput({ robotID, isGripper }) {
                                 hoverIndicator={true}>
                                 <b style={{ color: 'rgba(255, 255, 255, 0.85)' }}> {io.key} </b>
                                 <div >
-                                <NumberInput
-                                    value={io.value}
-                                    min={0}
-                                    max={Infinity}
-                                />
+                                    <NumberInput
+                                        value={io.value}
+                                        min={0}
+                                        max={Infinity}
+                                        onChange={(value) => updateItemSimpleProperty(robotID, { ...initialState, [io.key]: value })}
+                                    />
                                 </div>
 
                             </Box>
                         )
-                    })} 
+                    })}
 
 
                 </Collapse>
