ef57277ff0466fa63cf7c89de295e18bf99eda3d
Andrew Schoen <schoen.andrewj@gmail.com>
2022-08-24 10:45:58 -0500

    tauri!

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react";
 import { comlink } from "vite-plugin-comlink";
 import svgr from "vite-plugin-svgr";
 import wasmPack from 'vite-plugin-wasm-pack';
-// import macrosPlugin from "vite-plugin-babel-macros"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // macrosPlugin(),
     react(),
     svgr(),
     comlink(),
@@ -18,5 +16,4 @@ export default defineConfig({
   worker: {
     plugins: [comlink()],
   },
-  optimizeDeps: {disabled:false}
 });
