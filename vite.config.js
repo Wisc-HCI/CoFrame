167d7c6576f80253197d4367437aeb7e8ef6834e
Andrew Schoen <schoen.andrewj@gmail.com>
2023-03-14 22:20:10 +0100

    fixed build error

--- vite.config.js
+++ vite.config.js
@@ -2,45 +2,48 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { comlink } from "vite-plugin-comlink";
 import svgr from "vite-plugin-svgr";
-import wasmPack from 'vite-plugin-wasm-pack';
+import wasmPack from "vite-plugin-wasm-pack";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
-import path from 'path'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CoFrame/', 
+  base: "/CoFrame/",
   plugins: [
     react(),
     svgr(),
     comlink(),
-    wasmPack('./coframe-rust'),
+    wasmPack("./coframe-rust"),
     wasm(),
     topLevelAwait(),
   ],
   assetsInclude: ["**/*.gltf", "**/*.glb"],
   worker: {
     format: "es",
-    plugins: [
-      wasmPack('./coframe-rust'),
-      comlink(),
-      wasm(),
-      topLevelAwait(),
-    ],
+    plugins: [wasmPack("./coframe-rust"), comlink(), wasm(), topLevelAwait()],
+  },
+  esbuild: {
+    target: "es2020",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
   },
   resolve: {
     // mainFields:['module'],
     alias: {
-      '$simple-vp': path.resolve('.', '$simple-vp'),
+      "$simple-vp": path.resolve(".", "$simple-vp"),
     },
   },
   server: {
     fs: {
       // Allow serving files from one level up to the project root
-      allow: ['..'],
+      allow: [".."],
     },
   },
   test: {
     deps: { inline: ["@people_and_robots/lively", /\.wasm$/] },
-  }
+  },
 });
