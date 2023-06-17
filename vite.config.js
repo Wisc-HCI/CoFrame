f37f7ef092247f4c165b2e00e884e24f138933f1
Andrew Schoen <schoen.andrewj@gmail.com>
2023-06-17 08:25:58 -0500

    swc

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react-swc'
 import { comlink } from "vite-plugin-comlink";
 import svgr from "vite-plugin-svgr";
 import wasmPack from "vite-plugin-wasm-pack";
